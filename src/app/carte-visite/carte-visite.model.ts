592f256716c84f5d1bb36a4df605c16a61be525b
Nour daly <nourhouda.dali01@gmail.com>
2024-05-07 00:20:53 +0200

    first

---
+++ src/app/carte-visite/carte-visite.model.ts
@@ -0,0 +1,7 @@
+export class CarteVisite {
+    nom: string | undefined;
+    prenom: string | undefined;
+    entreprise: string | undefined;
+    email: string | undefined;
+    telephone: string | undefined;
+}
